b0dbd30d9205c7d5cb60342afe131d9ccc23f9d8
Aaron Rivas <aarivas793@gmail.com>
2024-01-25 19:05:51 -0800

    got OAuth working on frontend and added button on SignupComp

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import { useDispatch, useSelector } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
-
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import NavbarComp from '../components/NavbarComp.jsx';
 import TopTracksComp from '../components/TopTracksComp.jsx';
 import TopAlbumComp from '../components/TopAlbumComp.jsx';
@@ -12,32 +12,33 @@ import YearSliderComp from '../components/SliderComp.jsx';
 import SignupComp from '../components/SignupComp.jsx';
 // import LoginComp from '../components/LoginComp.jsx';
 
-import { fetchTopTracks, fetchTopArtists, setChosenTrack } from '../features/slice.js';
+import {
+  fetchTopTracks,
+  fetchTopArtists,
+  setChosenTrack,
+} from '../features/slice.js';
 // import DisplayYear from '../components/DisplayYear.jsx';
 import TopArtistsComp from '../components/TopArtistsComp.jsx';
 import '../../styles/index.scss';
 
-
-
 export function App() {
-
-      return (
-            
-            <>
-                  <Routes>
-                        <Route path="/" element={<SignupComp />} />
-                        {/* <Route path="/sign-up" element={<SignupComp />} /> */}
-                  </Routes>
-                  {/* <LogStateComp/>
+  return (
+    <>
+      <GoogleOAuthProvider clientId="61323721664-ut6009ddm9ce3bf41albrsi53gtbhvq2.apps.googleusercontent.com">
+        <Routes>
+          <Route path="/" element={<SignupComp />} />
+          {/* <Route path="/sign-up" element={<SignupComp />} /> */}
+        </Routes>
+      </GoogleOAuthProvider>
+      {/* <LogStateComp/>
       <NavbarComp/>
       <YearSliderComp/>
       <TopTracksComp/>
       <TopArtistsComp/>
       <TopAlbumComp/>
       <GraphComp/> */}
-            </>
-
-      )
+    </>
+  );
 }
 
 // This exports the entire file "App" or module.
