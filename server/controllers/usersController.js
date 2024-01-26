82906fc40b53b849532be6a842ece7566aa3e1e5
George Huang <gzh@princeton.alumni.com>
2024-01-25 18:07:22 -0800

    JWT creation/verification functionality added

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,22 +1,24 @@
 import { models } from '../models/model.js';
 import bcrypt from 'bcrypt';
 import JWT from 'jsonwebtoken';
+import dotenv from 'dotenv';
 
-
+dotenv.config({ path: '../.env.server' });
 const usersController = {};
 
 usersController.createUser = async (req, res, next) => {
     if (!req.body.email || !req.body.password) return res.status(400).json({err: "Please fill in all empty fields"})
     try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10)
-        await models.createUser(req.body.email, hashedPassword, req.body.name)
-        return next();
-    } catch {
-        return next({
-            log: 'userController.addUser - error creating user',
-            status: 400,
-            message: { err: 'userController.addUser - error creating user'},
-        })
+      const hashedPassword = await bcrypt.hash(req.body.password, 10)
+      await models.createUser(req.body.email, hashedPassword, req.body.name)
+      return next();
+    } catch(err) {
+      console.log(err)
+      return next({
+          log: 'userController.addUser - error creating user',
+          status: 400,
+          message: { err: 'userController.addUser - error creating user'},
+      })
     }
 
 }
@@ -38,7 +40,37 @@ usersController.authUser = async (req, res, next) => {
 }
 
 usersController.createJWT = (req, res, next) => {
-    const SECRET_KEY = 
+  const SECRET_KEY = process.env.JWT_KEY;
+  const token = JWT.sign(
+    { userId: res.locals.userId },
+    SECRET_KEY,
+    { expiresIn: '1h' }
+  )
+  res.cookie('token', token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: 'strict',
+    maxAge: 3600000
+  });
+  return next();
+}
+
+usersController.verifyJWT = (req, res, next) => {
+  const SECRET_KEY = process.env.JWT_KEY;
+  const token = req.cookies.token;
+  if (!token) {
+    return res.redirect('/login')
+  }
+  try {
+    const decoded = JWT.verify(token, SECRET_KEY);
+    res.locals.userId = decoded;
+  } catch {
+    return next({
+      log: 'userController.verifyJWT - error verifying token',
+      status: 400,
+      message: {err: 'userController.verifyJWT - error verifying token'}
+    })
+  }
 }
 
 export default usersController;
--- server/routes/usersRouter.js
+++ server/routes/usersRouter.js
@@ -3,12 +3,12 @@ const router = express.Router();
 import usersController from '../controllers/usersController.js';
 
 
-router.post('/login', usersController.authUser, (req, res) => {
+router.post('/login', usersController.authUser, usersController.createJWT, (req, res) => {
   res.sendStatus(200);
 })
 
 router.post('/signup', usersController.createUser, (req, res) => {
-  res.redirect('/login');
+  res.sendStatus(200);
 })
 
 router.post('/oauth', (req, res) => {
