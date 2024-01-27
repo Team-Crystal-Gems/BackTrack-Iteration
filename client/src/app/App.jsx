2168542596837ad76eb28575099ca8eacc71a9c8
heymistersin <99973059+heymistersin@users.noreply.github.com>
2024-01-27 01:27:10 -0500

    refactored, able to render / and /dashboard

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -3,15 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { useDispatch, useSelector } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
-import NavbarComp from '../components/NavbarComp.jsx';
-import TopTracksComp from '../components/TopTracksComp.jsx';
-import TopAlbumComp from '../components/TopAlbumComp.jsx';
-import GraphComp from '../components/GraphComp.jsx';
-import LogStateComp from '../components/LogStateComp.jsx';
-import YearSliderComp from '../components/SliderComp.jsx';
-import SignupComp from '../components/SignupComp.jsx';
-// import LoginComp from '../components/LoginComp.jsx';
 import AuthComp from '../components/AuthComp.jsx';
+import Dashboard from '../components/Dashboard.jsx';
 
 import {
   fetchTopTracks,
@@ -19,7 +12,6 @@ import {
   setChosenTrack,
 } from '../features/slice.js';
 // import DisplayYear from '../components/DisplayYear.jsx';
-import TopArtistsComp from '../components/TopArtistsComp.jsx';
 import '../../styles/index.scss';
 
 export function App() {
@@ -28,16 +20,9 @@ export function App() {
       <GoogleOAuthProvider clientId="61323721664-ut6009ddm9ce3bf41albrsi53gtbhvq2.apps.googleusercontent.com">
         <Routes>
           <Route path="/" element={<AuthComp />}/>
-          {/* <Route path="/dashboard" element={<Dashboard />}/> */}
+          <Route path="/dashboard" element={<Dashboard />}/>
         </Routes>
       </GoogleOAuthProvider>
-      {/* <LogStateComp/>
-      <NavbarComp/>
-      <YearSliderComp/>
-      <TopTracksComp/>
-      <TopArtistsComp/>
-      <TopAlbumComp/>
-      <GraphComp/> */}
     </>
   );
 }
--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
+import LogStateComp from './LogStateComp.jsx';
 import NavbarComp from './NavbarComp.jsx';
+import YearSliderComp from './SliderComp.jsx';
 import TopTracksComp from './TopTracksComp.jsx';
+import TopArtistsComp from './TopArtistsComp.jsx';
 import TopAlbumComp from './TopAlbumComp.jsx';
 import GraphComp from './GraphComp.jsx';
-import LogStateComp from './LogStateComp.jsx';
-import YearSliderComp from './SliderComp.jsx';
 
 const Dashboard = () => {
   return (
