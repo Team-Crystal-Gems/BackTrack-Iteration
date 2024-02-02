8fb7c15aab8677f7ee74b78c51c0dede8ba0f6cc
George Huang <gzh@princeton.alumni.com>
2024-02-01 18:36:50 -0800

    spotify api to get images for tracks

--- server/routes/tracksRouter.js
+++ server/routes/tracksRouter.js
@@ -1,14 +1,16 @@
 import express from 'express';
 const router = express.Router();
 import tracksController from '../controllers/tracksController.js';
+import spotifyController from '../controllers/spotifyController.js';
 
-router.get('/', tracksController.getTopTracks, (req, res) => {
+
+router.get('/', tracksController.getTopTracks, spotifyController.getTrackImage, (req, res) => {
   // console.log('in tracksRouter get /');
   return res.status(200).json(res.locals.topTracks);
 });
 
 //Ross added this to set up a route for front end slider to get tracks by year.
-router.get('/ByYear', tracksController.getTopTracksByYear, (req, res) => {
+router.get('/ByYear', tracksController.getTopTracksByYear, spotifyController.getTrackImage, (req, res) => {
   // console.log('in tracksRouter get /ByYear');
   return res.status(200).json(res.locals.topTracksByYear);
 });
