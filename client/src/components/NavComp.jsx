5a9dee41e86ff41f328b772fc701596701773626
heymistersin <99973059+heymistersin@users.noreply.github.com>
2024-02-03 02:26:22 -0500

    nav sizing correctly but not resizing well with smaller screen

--- client/src/components/NavComp.jsx
+++ client/src/components/NavComp.jsx
@@ -4,6 +4,7 @@ import logo from "../../assets/logo.png";
 import backtrack from "../../assets/backtrack.png";
 import eject from "../../assets/eject.png";
 import redButton from "../../assets/red_button.png";
+import play from "../../assets/play.png"
 
 const NavComp = () => {
 
@@ -25,18 +26,33 @@ const NavComp = () => {
 
   return(
     // <div>
-    <nav className="navBar">
-      <img src={logo} onClick={handleDashboard}/>
-      <div onClick={handleUpload}>
-        <img src={redButton}/>
-        <p className="navBar">Upload</p>
+    <nav className="nav">
+      {/* LEFT */}
+      <div className="left-nav"></div>
+
+      {/* CENTER */}
+      <div className="center-nav">
+      <img src={logo} />
       </div>
-      <div onClick={handleLogout}>
-        <img src={eject} />
-        <p className="navBar">Logout</p>
+
+      {/* RIGHT */}
+      <div className="right-nav">
+        <div id="dashboard-box" className="right-nav-btn" onClick={handleDashboard}>
+          <img id="play-icon" src={play} />
+          <p className="navBar">Dashboard</p>
+        </div>
+        <div id="upload-box"  className="right-nav-btn" onClick={handleUpload}>
+          <img src={redButton} />
+          <p className="navBar">Upload</p>
+        </div>
+        <div id="logout-box"  className="right-nav-btn" onClick={handleLogout}>
+          <img src={eject} />
+          <p className="navBar">Logout</p>
+        </div>
       </div>
+
     </nav>
-// </div>
+    // </div>
   )
 
 }
